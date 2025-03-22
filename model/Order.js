3824cf7a00f05107d5c7f35b39b9a79ece90ccef
teamparmjeet <teamparmjeet@gmail.com>
2025-03-22 14:39:10 +0530

    new

--- model/Order.js
+++ model/Order.js
@@ -15,17 +15,19 @@ const OrderSchema = new Schema(
         shippinpPincode: { type: String, required: true },
         paymentmod: { type: String, required: true },
         cftype: { type: String, required: true },
-        // state: { type: String, required: true },
-        // district: { type: String, required: true },
-        productgroup: { type: String, required: true },
-        // productcolor: { type: String, required: true },
-        product: { type: String, required: true },
+        productDetails: [
+            {
+                productgroup: { type: String, required: true },
+                product: { type: String, required: true },
+                quantity: { type: String, required: true }
+            }
+        ],
         salegroup: { type: String, required: true },
-        quantity: { type: String, required: true },
         shippingcharge: { type: String, required: true },
         netamount: { type: String, required: true },
         remarks: { type: String, },
         status: { type: Boolean, required: true, default: false },
+        totalsp: { type: String, required: true },
         defaultdata: { type: String, required: true, default: "Order" }
 
     },
@@ -34,7 +36,7 @@ const OrderSchema = new Schema(
 
 OrderSchema.pre("save", async function (next) {
     if (this.isNew) {
-        const lastorder = await mongoose.model("Ordertest7").findOne({}, {}, { sort: { orderNo: -1 } });
+        const lastorder = await mongoose.model("Ordertest110").findOne({}, {}, { sort: { orderNo: -1 } });
         if (lastorder && lastorder.orderNo) {
             this.orderNo = (parseInt(lastorder.orderNo, 10) + 1).toString();
         } else {
@@ -44,6 +46,6 @@ OrderSchema.pre("save", async function (next) {
     next();
 });
 const OrderModel =
-    mongoose.models.Ordertest7 || mongoose.model("Ordertest7", OrderSchema);
+    mongoose.models.Ordertest110 || mongoose.model("Ordertest110", OrderSchema);
 
 export default OrderModel
